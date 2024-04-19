4ae32c4c0898344f2cfe3cf232a66f64eec36204
Justin Nacu <JustAnalyze@users.noreply.github.com>
2024-04-19 18:51:03 +0800

    add todos and a dropdown list for choosing type of classification task

--- project.py
+++ project.py
@@ -1,31 +1,48 @@
 import customtkinter
 
-
-
 # create a class that has all the necessary GUI for training and predicting using a Image classifier model
 
-class GUI:
+class ModelBuilder:
     def __init__(self):
         customtkinter.set_default_color_theme('dark-blue')
+        customtkinter.set_appearance_mode("dark")
         self.root = customtkinter.CTk()
-        self.root.title("Image Classifier")
-        self.root.geometry("500x500")
+        self.root.title("Vision Forge")
+        self.root.geometry("600x300")
         
+        # Create tabs widgets for customizing the model and the data
         self.CreateTabs()
 
+        # Add Train Button for starting training
+        # TODO: Add button command for training
+        self.button = customtkinter.CTkButton(master=self.root, text="Train", command=None)
+        self.button.pack(pady=10)
+    # TODO: Create a CreateTabs Method for creating the widgets for customizing the model and the data
     def CreateTabs(self):
         # add tabs for Data loading, and Model Customization
         self.tabview = customtkinter.CTkTabview(self.root)
-        self.tabview.pack(padx=20, pady=20)
-        
-        self.tabview.add('Model')
-        self.tabview.add('Data')  
-        self.tabview.set('Model')  
+        self.tabview.pack(fill=customtkinter.BOTH, expand=True)
 
+        self.tabview.add('Model')
+        self.tabview.add('Data')
+        self.tabview.set('Model') 
+
+        # add widgets in the Model tab for customizing model architecture.
+        model_tab = self.tabview.tab('Model')
+        label = customtkinter.CTkLabel(master=model_tab, text="Task")
+        label.pack(pady=5)
+        # Add a ComboBox for choosing the loss function
+        classification_type_list = ['Binary Classification', 'Multiclass Classification']
+        self.classification_type = customtkinter.CTkComboBox(master=model_tab,
+                                                             values=classification_type_list)
+        self.classification_type.pack()
         
+    def Run(self):
+        self.root.mainloop()
+    
 def main():
-    gui = GUI()
-    gui.root.mainloop()
+    gui = ModelBuilder()
+    gui.Run()
 
 if __name__ == "__main__":
     main()
