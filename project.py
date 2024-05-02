8c4929ff92cb992db36166ce1c6d7ca92ccc100f
JustAnalyze <engr.nacu@gmail.com>
2024-05-02 22:07:38 +0800

    add the build model function in the main script.

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 import torch
+import torchvision
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import v2 as T
 from torch.utils.data import DataLoader
@@ -71,6 +72,58 @@ def data_setup(data_path: str,
                                  batch_size=batch_size)
 
     return train_dataloader, test_dataloader, train_data.classes
+
+
+def build_model(pretrained_model: str,
+                num_hidden_units: int,
+                output_shape: int,
+                device: str) -> Tuple[torchvision.transforms.Compose, torch.nn.Module]:
+    """
+    Build a neural network model using a pretrained model as a feature extractor.
+    
+    Args:
+        pretrained_model (str): Name of the pretrained model to use.
+        num_hidden_units (int): Number of hidden units in the new classifier layer.
+        output_shape (int): Number of output units in the new classifier layer.
+        device (str): Device where the model will be loaded ('cpu' or 'cuda').
+        
+    Returns:
+        model: Pretrained neural network model.
+        transforms: A callable transformation function that preprocesses input data.
+    """
+    
+    # Dictionary mapping model names to their corresponding torchvision models and weights
+    pretrained_models: dict[str, dict] = {
+        'EfficientNet': {
+            'model': torchvision.models.efficientnet_b3,
+            'weights': torchvision.models.EfficientNet_B3_Weights.DEFAULT
+        }
+    }
+    
+    # Get the weights and transformation function for the specified pretrained model
+    weights = pretrained_models[pretrained_model]['weights']
+    transforms = weights.transforms()  # Extract transformation function
+
+    
+    # Setup the model with pretrained weights and send it to the target device
+    model = pretrained_models[pretrained_model]['model'](weights=weights).to(device)
+
+    # Uncomment the line below to output the model (it's very long)
+    # print(model)
+    
+    # Freeze all base layers in the "features" section of the model (the feature extractor)
+    # by setting requires_grad=False
+    for param in model.features.parameters():
+        param.requires_grad = False
+        
+    # Recreate the classifier layer and seed it to the target device
+    model.classifier = torch.nn.Sequential(
+        torch.nn.Dropout(p=0.2, inplace=True,),
+        torch.nn.Linear(in_features=num_hidden_units,
+                        out_features=output_shape,  # use the length of class_names (one output unit for each class)
+                        bias=True)).to(device)
+    
+    return  model, transforms
 
 
 class ModelBuilderGUI:
@@ -492,12 +545,20 @@ class ModelBuilderGUI:
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
         torch.set_default_device(device)
         
+        # Build model
+        model, transforms = build_model(pretrained_model=self._settings_dict['model_settings']['pretrained_model'],
+                                        num_hidden_units=self._settings_dict['model_settings']['num_hidden_units'],
+                                        output_shape=self._settings_dict['data_settings']['num_classes'],
+                                        device=device)
+        
         # Load and preprocess the training and testing datasets.
         train_dataloader, test_dataloader, classes = data_setup(data_path=self._settings_dict['data_settings']['data_path'],
                                                                 batch_size=self._settings_dict['data_settings']['batch_size'],
                                                                 device=device,
+                                                                transform=transforms,  # use transforms used from training the pretrained model.
                                                                 normalize=True)
         
+        # train model
         ic(train_dataloader, test_dataloader, classes)
         
         
